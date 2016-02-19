8e74d3e32adae699e3b9b58d777e81ca859ac642
Dillon Jones <dillon.z.jones@gmail.com>
2016-02-19 12:23:52 -0500

    enable sourcemaps in chrome and safari

--- src/sass-inject.js
+++ src/sass-inject.js
@@ -61,10 +61,19 @@ const compile = scss => {
       }
       sass.compile(content, scss.options, result => {
         if (result.status === 0) {
-          const style = document.createElement('style');
-          style.textContent = result.text;
-          style.setAttribute('type', 'text/css');
-          document.getElementsByTagName('head')[0].appendChild(style);
+          if (result.hasOwnProperty("map")) {
+            const style = document.createElement('link');
+            style.setAttribute('type', 'text/css');
+            style.setAttribute('rel', 'stylesheet');
+            style.setAttribute('href', 'data:text/css;base64,' + btoa(result.text));
+            document.getElementsByTagName('head')[0].appendChild(style);
+          }
+          else {
+            const style = document.createElement('style');
+            style.textContent = result.text;
+            style.setAttribute('type', 'text/css');
+            document.getElementsByTagName('head')[0].appendChild(style);
+          }
           resolve('');
         } else {
           reject(result.formatted);
@@ -86,6 +95,28 @@ export default load => {
         options: {
           indentedSyntax,
           importer: { urlBase },
+          // Path to source map file
+          // Enables the source map generating
+          // Used to create sourceMappingUrl
+          sourceMapFile: 'style.css.map',
+          // Pass-through as sourceRoot property
+          sourceMapRoot: 'root',
+          // The input path is used for source map generation.
+          // It can be used to define something with string
+          // compilation or to overload the input file path.
+          // It is set to "stdin" for data contexts
+          // and to the input file on file contexts.
+          inputPath: 'stdin',
+          // The output path is used for source map generation.
+          // Libsass will not write to this file, it is just
+          // used to create information in source-maps etc.
+          outputPath: 'stdout',
+          // Embed included contents in maps
+          sourceMapContents: true,
+          // Embed sourceMappingUrl as data uri
+          sourceMapEmbed: true,
+          // Disable sourceMappingUrl in css output
+          sourceMapOmitUrl: false,
         },
       };
     })
